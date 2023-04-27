4e4ebe7d829c740670b94276d50050750dea87e6
Anna Darii <>
2023-04-27 17:40:41 +0200

    exercise file

---
+++ myFirstProgram.py
@@ -0,0 +1,37 @@
+# teachers = ["Anna", "Danylo"]
+# print(teachers[2])
+
+
+
+# age = input("What's your age?")
+# if age >= 18:
+#     print("You are an adult!")
+# else:
+#     print("You are still a minor.")
+
+
+
+# fruits = ["apple", "banana", "orange"]
+# for fruit in friuts:
+#     print(fruit)
+
+
+
+# fruits = ["apple", "banana", "orange"]
+# for fruit in friuts:
+#     print(a)
+
+
+
+# numbers = [1, 2, 3, 4, 5]
+# for i in range(len(numbers)):
+#     print(numbers[i+1])
+
+
+
+# a = 10
+# b = 5
+# if a < b and a > b:
+#     print("a is not equal to b")
+# else:
+#     print("a is equal to b")
